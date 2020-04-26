48fdab52d8445487744420668fb1e2bcfafea831
Lisa S <52213257+lisasce@users.noreply.github.com>
2020-04-26 15:19:13 +0200

    create form for adding/edit book

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,15 +1,76 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Select, Store} from '@ngxs/store';
+import {ActivatedRoute, Router} from '@angular/router';
+import {BookState} from '../states/book.state';
+import {AddBook, SetSelectedBook, UpdateBook} from '../actions/book.action';
+import {Observable, Subscription} from 'rxjs';
+import {Book} from '../models/Book';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.css']
 })
-export class FormComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class FormComponent implements OnInit, OnDestroy  {
+  @Select(BookState.getSelectedBook) selectedBook: Observable<Book>;
+  bookForm: FormGroup;
+  editBook = false;
+  private formSubscription: Subscription = new Subscription();
+  constructor(private fb: FormBuilder, private store: Store, private route: ActivatedRoute, private router: Router) {
+    this.createForm();
   }
 
+  ngOnInit(): void {
+    this.formSubscription.add(
+      this.selectedBook.subscribe(book => {
+        if (book) {
+          this.bookForm.patchValue({
+            bookId: book.bookId,
+            bookTitle: book.bookTitle,
+            bookAuthor: book.bookAuthor,
+            bookCategory: book.bookCategory,
+            bookFinished: book.finished
+          });
+          this.editBook = true;
+        } else {
+          this.editBook = false;
+        }
+      })
+    );
+  }
+
+  createForm() {
+    this.bookForm = this.fb.group({
+      bookId: [''],
+      bookTitle: ['', Validators.required],
+      bookAuthor: ['', Validators.required],
+      bookCategory: ['', Validators.required],
+      bookFinished: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (this.editBook) {
+      this.formSubscription.add(
+        this.store.dispatch(new UpdateBook(this.bookForm.value, this.bookForm.value.id)).subscribe(() => {
+          this.clearForm();
+        })
+      );
+    } else {
+      this.formSubscription.add(
+        this.formSubscription = this.store.dispatch(new AddBook(this.bookForm.value)).subscribe(() => {
+          this.clearForm();
+        })
+      );
+    }
+  }
+
+  clearForm() {
+    this.bookForm.reset();
+    this.store.dispatch(new SetSelectedBook(null));
+  }
+
+  ngOnDestroy(): void {
+  }
 }
--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import  {BookState} from '../states/book.state';
+import {Select, Store} from '@ngxs/store';
+import {Book} from '../models/Book';
+import {Observable} from 'rxjs';
+import {DeleteBook, GetBooks, SetSelectedBook} from '../actions/book.action';
 
 @Component({
   selector: 'app-table',
@@ -6,10 +11,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./table.component.css']
 })
 export class TableComponent implements OnInit {
-
-  constructor() { }
+  @Select(BookState.getBookList) books: Observable<Book[]>;
+  constructor(private store: Store) { }
 
   ngOnInit(): void {
+    this.store.dispatch(new GetBooks());
   }
-
+deleteBook(bookId: number) {
+  this.store.dispatch(new DeleteBook(bookId));
+}
+editBook(payload: Book) {
+    this.store.dispatch(new SetSelectedBook(payload));
+  }
 }
